acf8656c57f9d0313ba59b6f87eacabd869c0f9c
Anoushima <anoushima>
2026-01-19 12:58:56 +0530

    included changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header'
 import Footer from './components/footer'
+import { Link, Route, Routes } from 'react-router-dom'
+import AboutUs from './components/aboutus'
+import Flower from './components/flower'
 
 function App() {
  
@@ -11,7 +14,19 @@ function App() {
   return (
   
       <div>
+
+         
+        <Routes>
+         
+
+          <Route path='/about' element={<AboutUs />}></Route>
+          <Route path='/flower' element={<Flower />}></Route>
+        </Routes>
+
+        <Link to='/about'><button>About Us</button></Link>
+          <Link to='/flower'><button>Flower</button ></Link>
         <Header></Header>
+
         <Footer></Footer>
       </div>
       
---
+++ src/components/flower.tsx
@@ -0,0 +1,5 @@
+const Flower = () => {
+    return <h1>Flower Page</h1>;
+
+}
+export default Flower;
