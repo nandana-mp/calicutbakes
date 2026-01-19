179bc6741634c6181071fe13b529c279df22eb29
nandana <nandanakpz03@gmail.com>
2026-01-19 11:22:45 +0530

    first commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,8 @@
+const Footer =()=>{
+    return(
+        <div>
+            <h3>design and develop by cybersquare</h3>
+        </div>
+    )
+}
+export default Footer
---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+const Header =()=>{
+    return(
+        <div>
+    <h1 style={{alignItems:'center'}}>CalicutBakes</h1>
+        </div>
+    )
+}
+export default Header
